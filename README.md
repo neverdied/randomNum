4097fdf934e4bf2286cf09bf25d812ccf4f5eea0
周鑫文 <827773490@qq.com>
2022-09-09 17:20:01 +0800

    随机数生成器
    随机数生成器

---
+++ README.md
@@ -0,0 +1,3 @@
+# 随机数生成
+![image](./7day1.png)
+![image](http://39.99.172.143/img/7day1.PNG) 
---
+++ 随机数生成/随机数生成/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace 随机数生成
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                Random rd = new Random();
+                int num = rd.Next(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                label3.Text = "随机数是：" + num;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("请输入数字");
+            }
+            
+        }
+    }
+}
